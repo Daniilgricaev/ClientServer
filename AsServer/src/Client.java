acf0bfb05ad1b5b339c29a165376ec2fcd7be71d
Daniil <daniilgricaev805@gmail.com>
2026-05-08 22:27:13 +0700

    Add maven
    Add user registration

--- AsServer/src/Client.java
+++ AsServer/src/Client.java
@@ -6,15 +6,17 @@ import java.net.UnknownHostException;
 public class Client {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name :");
-        String userName = scanner.nextLine();
+//        System.out.println("Enter your name :");
+//        String userName = scanner.nextLine();
+        User user = new User(null, null, null);
+        user.reg();
 
         try (Socket socket = new Socket("localhost", 8080);
              BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
              PrintWriter pw = new PrintWriter(socket.getOutputStream(),true);
              BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 
-            pw.println(userName);
+            pw.println(user.name);
 
             System.out.println("Client connected to socket.\n");
             System.out.println("Client writing channel = oos & reading channel = ois initialized.");
